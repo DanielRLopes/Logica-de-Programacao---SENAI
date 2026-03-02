a8c029d11e6b9c9b7a04e843081b48802573d21c
DANIEL RAMOS LOPES <daniel_ramos-lopes@estudante.sesisenai.org.br>
2026-03-02 20:44:54 -0300

    Atividade 2 completa

--- 02.js
+++ 02.js
@@ -1,4 +1,44 @@
 console.log(`
 - - - CRIANDO E ACESSANDO ARRAYS - - -
-`)
+`);
 
+let cores = ["Amarelo", "Vermelho", "Azul", "Roxo"];
+
+console.log(`
+${cores[0]} , ${cores[3]}
+`);
+
+console.log(`
+- - - MÉTODOS BÁSICOS - - -
+`);
+cores.push("Branco");
+cores.shift(); //   shift+() para remover do início
+console.log(`
+${cores} 
+${cores.length}
+`);
+
+console.log(`
+- - - NÚMEROS - - -
+`);
+
+const Numbers = [0, 1, 2, 3, 4];
+
+console.log(`${Numbers} : ${Numbers.length}`);
+
+console.log(`
+- - - TODOS OS TIPOS - - -
+`);
+
+let todoTipo = ["Verdade", 777, null, true, undefined];
+console.log(`${todoTipo} : ${todoTipo.length}`);
+
+console.log(`
+- - - MUDANDO A LISTA - - -
+`);
+
+const meuArray = [20, 21, "Kael", "Zaozao", true, false, null, undefined];
+console.log(meuArray);
+    meuArray.pop(); meuArray.shift();
+console.log(meuArray)
+    meuArray.unshift(20); meuArray.push(undefined)
